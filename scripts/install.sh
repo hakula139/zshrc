cbbf1ba92b3b07e29bddaa141212c909c470f791
Hakula Chen <i@hakula.xyz>
2022-10-12 17:17:35 +0800

    chore: update install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,11 +7,13 @@ cp .zshrc ~
 
 # Install theme powerlevel10k
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 cp .p10k.zsh ~
 
 # Install plugins
 
 brew install autojump
+brew install fzf
+brew install git-lfs
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
